b1f8e93082de52a3f6f1dba624420130e84c811e
unknown <ftemewh@gmail.com>
2025-02-09 15:50:06 +0330

    tree added in grouping, need modification

--- src/pages/product-info/grouping/index.tsx
+++ src/pages/product-info/grouping/index.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import type { DataNode } from 'antd/es/tree';
+
+import { TreeSelect } from 'antd';
+import React, { useState } from 'react';
 
 function Grouping() {
-  return <div>Grouping</div>;
+  const initTreeData: DataNode[] = [
+    { title: 'Expand to load', key: '0' },
+    { title: 'Expand to load', key: '1' },
+    { title: 'Tree Node', key: '2', isLeaf: true },
+  ];
+
+  const [treeData, setTreeData] = useState(initTreeData);
+
+  return (
+    <div>
+      <TreeSelect showSearch treeNodeFilterProp="title" treeData={treeData} />
+    </div>
+  );
 }
 
 export default Grouping;
