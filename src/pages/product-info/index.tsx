3ae5cd9ec833dd951c7d8b80d954be62af4bfe8d
FatemehAria <ftemewh@gmail.com>
2025-02-10 13:59:55 +0330

    border bottom added for active tab, tabs modified

--- src/pages/product-info/index.tsx
+++ src/pages/product-info/index.tsx
@@ -12,9 +12,15 @@ function ProductInfo() {
   return (
     <div>
       <div className="tabs">
-        <p onClick={() => setTab(0)}>اطلاعات اصلی</p>
-        <p onClick={() => setTab(1)}>ویژگی ها</p>
-        <p onClick={() => setTab(2)}>گروه بندی</p>
+        <p onClick={() => setTab(0)} className={`single-tab ${tab === 0 ? 'active-tab' : ''}`}>
+          اطلاعات اصلی
+        </p>
+        <p onClick={() => setTab(1)} className={`single-tab ${tab === 1 ? 'active-tab' : ''}`}>
+          ویژگی ها
+        </p>
+        <p onClick={() => setTab(2)} className={`single-tab ${tab === 2 ? 'active-tab' : ''}`}>
+          گروه بندی
+        </p>
       </div>
 
       {tab === 0 ? <MainInfo /> : tab === 1 ? <Specifications /> : <Grouping />}
