f520615ec24e7d1f8f59311576a4599e3c4b6bea
FatemehAria <ftemewh@gmail.com>
2025-02-10 17:27:17 +0330

    customer info for added

--- src/pages/costumer-info/index.tsx
+++ src/pages/costumer-info/index.tsx
@@ -1,7 +1,59 @@
+import type { MyFormOptions } from '@/components/core/form';
+
 import React from 'react';
 
+import FormLayout from '../layout/form-layout';
+
+const costumerInfoFormOptions: MyFormOptions = [
+  {
+    name: 'costumer-info-type',
+    label: 'نوع :',
+    type: 'select',
+    innerProps: { placeholder: '' },
+    options: [],
+  },
+  {
+    name: 'costumer-info-person-type',
+    label: 'نوع شخص:',
+    type: 'select',
+    innerProps: { placeholder: '' },
+    options: [
+      { label: 'حقیقی', value: 'Haghighi' },
+      { label: 'حقوقی', value: 'Hoghooghi' },
+    ],
+  },
+  {
+    name: 'costumer-info-factor-code',
+    label: 'کد فاکتور:',
+    type: 'input',
+    innerProps: { placeholder: '' },
+  },
+  {
+    name: 'costumer-info-org-code',
+    label: 'کد اداری:',
+    type: 'input',
+    innerProps: { placeholder: '' },
+  },
+  {
+    name: 'costumer-info-active',
+    label: 'وضعیت:',
+    type: 'radio',
+    options: [
+      { label: 'فعال', value: true },
+      { label: 'غیرفعال', value: false },
+    ],
+  },
+];
+
 function CostumerInfo() {
-  return <div>CostumerInfo</div>;
+  return (
+    <FormLayout
+      FormOptions={costumerInfoFormOptions}
+      layoutDir="vertical"
+      submitForm={values => console.log('Submitted values:', values)}
+      isGrid={false}
+    />
+  );
 }
 
 export default CostumerInfo;
