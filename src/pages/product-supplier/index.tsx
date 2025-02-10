619b4c8a628d166fdacbff64f235c42ba23e947d
FatemehAria <ftemewh@gmail.com>
2025-02-10 17:15:35 +0330

    product supplier form added

--- src/pages/product-supplier/index.tsx
+++ src/pages/product-supplier/index.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import type { MyFormOptions } from '@/components/core/form';
+
+import React from 'react';
+import FormLayout from '../layout/form-layout';
+
+const productSupplierFormOptions: MyFormOptions = [
+  {
+    name: 'product-supplier-type',
+    label: 'نوع شخص:',
+    type: 'select',
+    innerProps: { placeholder: '' },
+    options: [
+      { label: 'حقیقی', value: 'Haghighi' },
+      { label: 'حقوقی', value: 'Hoghooghi' },
+    ],
+  },
+  {
+    name: 'product-supplier-province',
+    label: 'استان:',
+    type: 'select',
+    innerProps: { placeholder: '' },
+  },
+];
 
 function ProductSupplier() {
   return (
-    <div>ProductSupplier</div>
-  )
+    <FormLayout
+      FormOptions={productSupplierFormOptions}
+      layoutDir="vertical"
+      submitForm={values => console.log('Submitted values:', values)}
+      isGrid={false}
+    />
+  );
 }
 
-export default ProductSupplier
+export default ProductSupplier;
