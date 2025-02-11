885c6ad336b1fda448a81d60b1860f5fd13048a7
unknown <ftemewh@gmail.com>
2025-02-11 12:35:39 +0330

    person/company info languages added

--- src/pages/person-company-info/index.tsx
+++ src/pages/person-company-info/index.tsx
@@ -2,103 +2,106 @@ import type { MyFormOptions } from '@/components/core/form';
 
 import React from 'react';
 
+import { useLocale } from '@/locales';
+
 import FormLayout from '../layout/form-layout';
 
-const personCompanyFormOptions: MyFormOptions = [
-  {
-    name: 'person-company-type',
-    label: 'نوع شخص:',
-    type: 'select',
-    innerProps: { placeholder: '' },
-    options: [
-      { label: 'حقیقی', value: 'Haghighi' },
-      { label: 'حقوقی', value: 'Hoghooghi' },
-    ],
-  },
-  {
-    name: 'person-company-firstname-persian',
-    label: 'نام فارسی:',
-    type: 'input',
-    innerProps: { placeholder: '' },
-  },
-  {
-    name: 'person-company-firstname-english',
-    label: 'نام انگلیسی:',
-    type: 'input',
-    innerProps: { placeholder: '' },
-  },
-  {
-    name: 'person-company-lastname-persian',
-    label: 'نام خانوادگی فارسی:',
-    type: 'input',
-    innerProps: { placeholder: '' },
-  },
-  {
-    name: 'person-company-lastname-english',
-    label: 'نام خانوادگی انگلیسی:',
-    type: 'input',
-    innerProps: { placeholder: '' },
-  },
-  {
-    name: 'person-company-email',
-    label: 'ایمیل:',
-    type: 'input',
-    innerProps: { placeholder: '' },
-  },
-  {
-    name: 'person-company-mobile',
-    label: 'موبایل:',
-    type: 'input',
-    innerProps: { placeholder: '' },
-  },
-  {
-    name: 'person-company-phonenumber',
-    label: 'تلفن:',
-    type: 'input',
-    innerProps: { placeholder: '' },
-  },
-  {
-    name: 'person-company-nationalID',
-    label: 'شناسه/کدملی:',
-    type: 'input',
-    innerProps: { placeholder: '' },
-  },
-  {
-    name: 'person-company-province',
-    label: 'استان:',
-    type: 'select',
-    innerProps: { placeholder: '' },
-  },
-  {
-    name: 'person-company-city',
-    label: 'شهر:',
-    type: 'select',
-    innerProps: { placeholder: '' },
-  },
-  {
-    name: 'person-company-address',
-    label: 'آدرس:',
-    type: 'input',
-    innerProps: { placeholder: '' },
-  },
-  {
-    name: 'person-company-postalCode',
-    label: 'کدپستی:',
-    type: 'input',
-    innerProps: { placeholder: '' },
-  },
-  {
-    name: 'person-company-active',
-    label: 'وضعیت:',
-    type: 'radio',
-    options: [
-      { label: 'فعال', value: true },
-      { label: 'غیرفعال', value: false },
-    ],
-  },
-];
-
 function CompanyPersonInfo() {
+  const { formatMessage } = useLocale();
+  const personCompanyFormOptions: MyFormOptions = [
+    {
+      name: 'person-company-type',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.personType' })}`,
+      type: 'select',
+      innerProps: { placeholder: '' },
+      options: [
+        { label: 'حقیقی', value: 'Haghighi' },
+        { label: 'حقوقی', value: 'Hoghooghi' },
+      ],
+    },
+    {
+      name: 'person-company-firstname-persian',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.firstnamePer' })}`,
+      type: 'input',
+      innerProps: { placeholder: '' },
+    },
+    {
+      name: 'person-company-firstname-english',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.firstnameEng' })}`,
+      type: 'input',
+      innerProps: { placeholder: '' },
+    },
+    {
+      name: 'person-company-lastname-persian',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.lastnamePer' })}`,
+      type: 'input',
+      innerProps: { placeholder: '' },
+    },
+    {
+      name: 'person-company-lastname-english',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.lastnameEng' })}`,
+      type: 'input',
+      innerProps: { placeholder: '' },
+    },
+    {
+      name: 'person-company-email',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.email' })}`,
+      type: 'input',
+      innerProps: { placeholder: '' },
+    },
+    {
+      name: 'person-company-mobile',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.mobile' })}`,
+      type: 'input',
+      innerProps: { placeholder: '' },
+    },
+    {
+      name: 'person-company-phonenumber',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.phone' })}`,
+      type: 'input',
+      innerProps: { placeholder: '' },
+    },
+    {
+      name: 'person-company-nationalID',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.nationalID' })}`,
+      type: 'input',
+      innerProps: { placeholder: '' },
+    },
+    {
+      name: 'person-company-province',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.province' })}`,
+      type: 'select',
+      innerProps: { placeholder: '' },
+    },
+    {
+      name: 'person-company-city',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.city' })}`,
+      type: 'select',
+      innerProps: { placeholder: '' },
+    },
+    {
+      name: 'person-company-address',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.address' })}`,
+      type: 'input',
+      innerProps: { placeholder: '' },
+    },
+    {
+      name: 'person-company-postalCode',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.postalCode' })}`,
+      type: 'input',
+      innerProps: { placeholder: '' },
+    },
+    {
+      name: 'person-company-active',
+      label: `${formatMessage({ id: 'app.personComapnyInfo.status' })}`,
+      type: 'radio',
+      options: [
+        { label: `${formatMessage({ id: 'app.personComapnyInfo.status.active' })}`, value: true },
+        { label: `${formatMessage({ id: 'app.personComapnyInfo.status.deactive' })}`, value: false },
+      ],
+    },
+  ];
+
   return (
     <FormLayout
       FormOptions={personCompanyFormOptions}
