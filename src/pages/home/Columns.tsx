a2f9500f586a2a14235d897b314684bb73600322
FatemehAria <ftemewh@gmail.com>
2025-02-28 18:56:29 +0330

    mode defined for items and category

--- src/pages/home/Columns.tsx
+++ src/pages/home/Columns.tsx
@@ -56,6 +56,7 @@ export const Columns = (
           handleCellChange={handleCellChange}
           initialOptions={[]}
           debounceTime={5000}
+          mode="tags"
         />
       ),
     },
@@ -76,6 +77,7 @@ export const Columns = (
           handleCellChange={handleCellChange}
           initialOptions={[]}
           debounceTime={5000}
+          mode="tags"
         />
       ),
     },
