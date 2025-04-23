c2a161a0486c7313989deced405dfba16acbaa74
zahraariani <zhrkrm9@gmail.com>
2025-04-23 21:29:19 +0330

    stuffModified and categoryModified set to null when local empty

--- src/pages/home/util.ts
+++ src/pages/home/util.ts
@@ -249,8 +249,8 @@ export function mapRowToApiDetail(
     priceSaleRounded: parseFloat(String(row.itemSalePriceRounded).replace(/,/g, '')) || 0,
     priceSaleFinal: row.finalSalePrice || 0,
     costTotal: row.totalPriceWithoutFactors || 0,
-    existenceCategoryTitleModified: localStorage.getItem(`editedOption-category`) || '',
-    stuffParentTitleModified: localStorage.getItem(`editedOption-items`) || '',
+    existenceCategoryTitleModified: localStorage.getItem(`editedOption-category`) || null,
+    stuffParentTitleModified: localStorage.getItem(`editedOption-items`) || null,
     insurancePrice: row.insurancePriceForRecord,
   };
 }
