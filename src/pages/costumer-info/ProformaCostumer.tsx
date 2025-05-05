079f18d9a8a086632158ae98b7e78297798dab40
unknown <ftemewh@gmail.com>
2025-05-05 09:17:30 +0330

    ProformaCostumer in costumer-info province and city made searchable

--- src/pages/costumer-info/ProformaCostumer.tsx
+++ src/pages/costumer-info/ProformaCostumer.tsx
@@ -143,6 +143,8 @@ function ProformaCostumer({ onCustomerSubmit }: ProformaCostumerProps) {
       label: `${formatMessage({ id: 'app.personComapnyInfo.province' })}`,
       type: 'select',
       innerProps: {
+        showSearch: true,
+        filterOption: (input: string, option: any) => option.label.toLowerCase().includes(input.toLowerCase()),
         placeholder: '',
         onChange: (value: string) => {
           getCity(setCityList, value);
@@ -156,6 +158,8 @@ function ProformaCostumer({ onCustomerSubmit }: ProformaCostumerProps) {
       label: `${formatMessage({ id: 'app.personComapnyInfo.city' })}`,
       type: 'select',
       innerProps: {
+        showSearch: true,
+        filterOption: (input: string, option: any) => option.label.toLowerCase().includes(input.toLowerCase()),
         placeholder: '',
         // onChange: (value: any) => {
         //   // console.log('City changed:', value);
