3ac0324d8763749ab0e81630696fa833c53aa114
zahraariani <zhrkrm9@gmail.com>
2025-05-07 23:17:31 +0330

    baseUrl changed to localhost

--- src/utils/axios-config.ts
+++ src/utils/axios-config.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const customAxiosInstance = axios.create({
-  baseURL: 'https://edgex.liara.run/api',
+  baseURL: 'https://localhost:7214/api',
 });
 // https://localhost:7214/api
+// https://edgex.liara.run/api
